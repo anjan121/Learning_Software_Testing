528f4475f3c8c6b09f40cb83dd03cd5023bce53c
anjan121 <anjansa35@gmail.com>
2026-06-29 12:50:31 +0530

    Added the sample program

---
+++ src/software_Testing/Simple_program.java
@@ -0,0 +1,22 @@
+package software_Testing;
+
+public class Simple_program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   System.out.println(5);
+		   System.out.println(2.3);
+		   System.out.println('a');
+		   System.out.println('5');  
+		   System.out.println('@');   
+		   
+		   System.out.println("Bharath");  
+		   System.out.println("Anjan");    
+		   System.out.println("1234");  
+		   System.out.println("abc123 #$%^%");
+		   
+		   System.out.println(true);
+		   System.out.println(false);
+	}
+
+}
